f2e035bd8f20e28749ef730b1a2c27386933b0ab
Alex <alex@ubuntu.(none)>
2013-03-16 11:24:29 +0100

    file size calculator

---
+++ size.c
@@ -0,0 +1,28 @@
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+  int size=0;
+  int fd;
+
+  if(argc!= 2){
+    printf("\n\nUsage: size 'file'\n\n");
+    exit(1);
+  }
+  
+  else if (fd = (open(argv[1],0,O_RDONLY))==-1){
+    printf("\n\n File not found!\n\n");
+    exit(1);
+  }
+  else{
+    fd = (open(argv[1],0,O_RDONLY));
+    size = (int)(lseek(fd,0,SEEK_END));
+    printf("\n\nsize : %d\n\n", size);
+  }
+  
+
+  return 0;
+}
